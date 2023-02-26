bb909aaee1da0cc941a45470d75604d216de8c5a
dnguye234 <dnguye2@uw.edu>
2023-02-25 22:45:34 -0800

    complete report

--- chart1_example.R
+++ chart1_example.R
@@ -1,6 +1,21 @@
+spl_df <- read.csv("~/Desktop/2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
 x_values <- seq(1, 3)
-y_values <- seq(1,3)
+y_values <- seq(1, 3)
 
+# compare line graph of physical copies over the month of each year
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+library(dplyr)
+library(scales)
+
+c1_df <- spl_df %>%
+  filter(MaterialType %in% c("BOOK", "EBOOK", "AUDIOBOOK")) %>%
+  group_by(CheckoutMonth, MaterialType) %>%
+  summarise(avg_checkout = mean(Checkouts))
+
+ggplot(c1_df) +
+  geom_line(aes(x = CheckoutMonth, y = avg_checkout, color = MaterialType)) +
+  labs(title = "Average Checkouts between Books, Ebooks, and Audiobooks", 
+       caption = "Data Provided by the Seattle Public Library, from 2017 to 2023", 
+       x = "month", 
+       y = "average checkouts") +
+  scale_x_continuous(breaks = seq(1, 12, 1))
--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,36 @@
 x_values <- seq(1, 3)
-y_values <- seq(1,3)
+y_values <- seq(1, 3)
+
+spl_df <- read.csv("~/Desktop/2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
 
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+
+c2_df <- spl_df %>%
+  filter(Creator %in% c("Murata, Sayaka, 1979-", "Sayaka Murata")) %>%
+  group_by(CheckoutMonth, Title) %>%
+  summarise(total_checkout = sum(Checkouts))
+
+c2_clean_1 <- c2_df %>%
+  filter(Title %in% c("Convenience Store Woman", "Convenience Store Woman (unabridged)", "Convenience Store Woman (Unabridged)", "Convenience store woman / Sayaka Murata ; translated from the Japanese by Ginny Tapley Takemori.", "Convenience Store Woman: A Novel")) %>% 
+  summarise(total_checkout = sum(total_checkout)) %>%
+  mutate(Title = "Convenience Store Woman")
+
+c2_clean_2 <- c2_df %>%
+  filter(Title %in% c("Earthlings : a novel / Sayaka Murata ; translated from the Japanese by Ginny Tapley Takemori.", "Earthlings: A Novel", "Earthlings: A Novel (unabridged)", "Earthlings: A Novel (unabridged) (Unabridged)")) %>% 
+  summarise(total_checkout = sum(total_checkout)) %>%
+  mutate(Title = "Earthlings: A Novel")
+
+c2_clean_3 <- c2_df %>%
+  filter(Title %in% c("Life ceremony : stories / Sayaka Murata ; translated from the Japanese by Ginny Tapley Takemori.", "Life Ceremony: Stories", "Life Ceremony: Stories (unabridged)")) %>% 
+  summarise(total_checkout = sum(total_checkout)) %>%
+  mutate(Title = "Life Ceremony: Stories")
+
+c2_df_join <- rbind(c2_clean_1, c2_clean_2, c2_clean_3)
+
+ggplot(c2_df_join) +
+  geom_line(aes(x = CheckoutMonth, y = total_checkout, color = Title)) +
+  labs(title = "Total Checkouts Of Works by Sayaka Murata", 
+       caption = "Data Provided by the Seattle Public Library, from 2017 to 2023", 
+       x = "month", 
+       y = "total checkouts") +
+  scale_x_continuous(breaks = seq(1, 12, 1))
